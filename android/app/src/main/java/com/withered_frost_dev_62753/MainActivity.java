954c91a538fdaacad4de39b1226e9e765d15ccf4
Crowdbotics <engineering@crowdbotics.com>
2023-02-11 00:14:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/withered_frost_dev_62753/MainActivity.java
@@ -0,0 +1,15 @@
+package com.withered_frost_dev_62753;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "withered_frost_dev_62753";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-frost-dev-62753",
+  url: "https://withered-frost-dev-62753.botics.co",
+  api: "https://withered-frost-dev-62753.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
